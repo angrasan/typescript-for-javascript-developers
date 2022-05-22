3d4ceaf0f17ce293b9ca3e6c971a948e339c60eb
angrasan <a0n9g2r6a@outlook.jp>
2022-05-22 21:46:42 +0900

    namespace

---
+++ src/namespaces.ts
@@ -0,0 +1,36 @@
+export {};
+
+namespace Japanese {
+  export namespace Tokyo {
+    export class Person {
+      constructor(public name: string) {}
+    }
+  }
+
+  export namespace Osaka {
+    export class Person {
+      constructor(public name: string) {}
+    }
+  }
+}
+
+
+namespace English {
+  export class Person {
+    constructor(
+      public firstName: string,
+      public middleName: string,
+      public lastName: string
+    ) {}
+  }
+}
+
+
+const me = new Japanese.Tokyo.Person("Ebizo");
+console.log(me.name);
+
+const meOsaka = new Japanese.Osaka.Person("Ichikawa");
+console.log(meOsaka.name);
+
+const jackson = new English.Person('Michael', 'Joseph', 'Jackson');
+console.log(jackson);
